87bc693bfda8f6ee2dc19a96929d1efd1e8fd677
Thomas Giannetti-Fakhoury <72355918+thomasgiannetti@users.noreply.github.com>
2024-10-07 12:21:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
 
 # Set page config for the entire app
 st.sidebar.header("Navigation.")
 
 st.title("Welcome to the Delivery and Customer Map App")
 st.write("Use the sidebar to navigate between the Delivery Routes and Customer Map pages.")
+
+switch_page("Delivery Routes")
+
+
