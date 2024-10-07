e556d30c19a12febf0da86f925f0f6075a9d5e91
Thomas Giannetti-Fakhoury <72355918+thomasgiannetti@users.noreply.github.com>
2024-10-07 18:31:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ st.set_page_config(page_title="My Multipage App", page_icon="🌍", layout="wide
 
 # Set page config for the entire app
 st.sidebar.header("Navigation.")
+# Main Page
+st.title("Main Page")
+st.sidebar.success("Select a page above.")
 
 st.title("Welcome to the Delivery and Customer Map App")
 st.write("Use the sidebar to navigate between the Delivery Routes and Customer Map pages.")
