73999635e54512ce2df3bd54b021ae84addf712f
Thomas Giannetti-Fakhoury <72355918+thomasgiannetti@users.noreply.github.com>
2024-10-07 11:30:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 # Set page config for the entire app
 st.set_page_config(page_title="My Multipage App", page_icon="🌍", layout="wide")
+st.sidebar.header("Navigation.")
 
 st.title("Welcome to the Delivery and Customer Map App")
 st.write("Use the sidebar to navigate between the Delivery Routes and Customer Map pages.")
