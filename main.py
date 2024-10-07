1d054b115842fda3504b7f78ebfd99eb2d2cf387
Thomas Giannetti-Fakhoury <72355918+thomasgiannetti@users.noreply.github.com>
2024-10-07 12:09:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,5 @@ import streamlit as st
 # Set page config for the entire app
 st.sidebar.header("Navigation.")
 
-def admin_sidebar():
-    with st.sidebar:
-        st.page_link('./pages/delivery_routes.py', label='Delivery Route')
-        st.page_link('./pages/customer_map.py', label='Customer Map')
-        st.button('logout', key='logout')
-
-admin_sidebar()
-
-
 st.title("Welcome to the Delivery and Customer Map App")
 st.write("Use the sidebar to navigate between the Delivery Routes and Customer Map pages.")
