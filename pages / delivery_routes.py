19e194eaaf827408f934b3cfe628c2ddfdaff077
Thomas Giannetti-Fakhoury <72355918+thomasgiannetti@users.noreply.github.com>
2024-10-07 11:29:09 +0000

    Update  delivery_routes.py

--- pages / delivery_routes.py
+++ pages / delivery_routes.py
@@ -15,6 +15,8 @@ from datetime import datetime, timedelta
 import googlemaps
 import io
 
+st.set_page_config(page_title="Delivery Route", page_icon="📊")
+
 SSH_USERNAME = st.secrets["SSH_USERNAME"]
 SSH_PASSWORD = st.secrets["SSH_PASSWORD"]
 DB_USER = st.secrets["DB_USER"]
