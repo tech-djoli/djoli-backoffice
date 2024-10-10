fba323984b6983691c1b0f95b11da675156bc166
Thomas Giannetti-Fakhoury <72355918+thomasgiannetti@users.noreply.github.com>
2024-10-10 02:54:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ if page == "Main Page":
     st.title("Welcome to the Delivery and Customer Map App")
     st.write("Use the sidebar to navigate between the Delivery Routes and Customer Map pages.")
 elif page == "Delivery Routes":
-    import pages.1_deliveryroutes.py as page1
+    import pages.deliveryroutes.py as page1
     page1.show_page()
 elif page == "Customer Map":
-    import pages.2_customermap.py as page2
+    import pages.customermap.py as page2
     page2.show_page()
 elif page == "Producer Map":
-    import pages.3_producermap.py as page3
+    import pages.producermap.py as page3
     page3.show_page()
