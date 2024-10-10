9b84ca600282b21d0747471f00adf8b222b87568
thomas.giannetti@gmail.com <thomas.giannetti@gmail.com>
2024-10-10 02:13:31 +0000

    test main page

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ st.set_page_config(page_title="My Multipage App", page_icon="🌍", layout="wide
 st.sidebar.header("Navigation.")
 # Main Page
 st.title("Main Page")
-st.sidebar.success("Select a page above.")
 
 st.title("Welcome to the Delivery and Customer Map App")
 st.write("Use the sidebar to navigate between the Delivery Routes and Customer Map pages.")
