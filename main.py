b5e0fc8246490c3e919d959cd60b7ee81dc0bd29
thomas.giannetti@gmail.com <thomas.giannetti@gmail.com>
2024-10-10 03:45:24 +0000

    adding emoji

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.set_page_config(page_title="main", page_icon="🏠")
-st.title("Welcome to the Delivery and Customer Map App")
+st.title("Welcome to Djoli's Data Tools 📊")
 
