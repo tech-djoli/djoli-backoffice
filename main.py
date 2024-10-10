5ebe0088cc25858c74c71cdf138a1bf21949446d
thomas.giannetti@gmail.com <thomas.giannetti@gmail.com>
2024-10-10 03:16:14 +0000

    v2 main file

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title="My Multipage App", page_icon="🌍", layout="wide")
+st.set_page_config(page_title="main", page_icon="🏠")
+st.title("Welcome to the Delivery and Customer Map App")
 
-st.sidebar.header("Navigation")
-page = st.sidebar.selectbox("Go to", ["Main Page", "Delivery Routes", "Customer Map", "Producer Map"])
-
-if page == "Main Page":
-    st.title("Welcome to the Delivery and Customer Map App")
-    st.write("Use the sidebar to navigate between the Delivery Routes and Customer Map pages.")
-elif page == "Delivery Routes":
-    import pages.deliveryroutes.py as page1
-    page1.show_page()
-elif page == "Customer Map":
-    import pages.customermap.py as page2
-    page2.show_page()
-elif page == "Producer Map":
-    import pages.producermap.py as page3
-    page3.show_page()
